8d95096a1daf3e440a427f316751ffefb75e9636
Mauricio Navarro <julianmn15@gmail.com>
2025-02-14 18:51:02 -0300

    agregando alertas y reporte de altas para las internaciones

--- internacion/admin.py
+++ internacion/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Internacion,Atencion,Seguimiento,Medicacion,SignosVitales
+from .models import Internacion,Diagnostico,Seguimiento,Medicacion,SignosVitales
 # Register your models here.
 admin.site.register(Internacion)
-admin.site.register(Atencion)
+admin.site.register(Diagnostico)
 admin.site.register(Seguimiento)
 admin.site.register(Medicacion)
 admin.site.register(SignosVitales)
